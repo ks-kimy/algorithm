2b926b9e87d8bf11b58b5fe48e120f7a3b13c5e1
kks <kkisung2657@gmail.com>
2024-03-21 01:09:38 +0900

    easy bfs

--- fugitive/1953.py
+++ fugitive/1953.py
@@ -4,20 +4,18 @@ from collections import deque
 sys.stdin = open("input.txt", "r")
 
 def bfs(R,C):
-    q.append((R,C,1))
+    q.append((R,C))
 
     while q:
-        row , col, time = q.popleft()
+        row , col= q.popleft()
         way = tunel[row][col]
         for drow, dcol in ways[way]:
             nrow = row + drow
             ncol = col + dcol
-            if 0<= nrow <N and 0<= ncol <M and tunel[nrow][ncol]:
-                if not visited[nrow][ncol]:
-                    visited[nrow][ncol] = time
-                    q.append((nrow,ncol,time+1))
-        if time == L:
-            return
+            if 0<= nrow <N and 0<= ncol <M and tunel[nrow][ncol]>0 and visited[nrow][ncol] == 0:
+                if [-drow, -dcol] in ways[tunel[nrow][ncol]]:
+                    q.append((nrow,ncol))
+                    visited[nrow][ncol] = visited[row][col]+1
 
 
 T = int(input())
@@ -26,13 +24,14 @@ for tc in range(1, T+1):
     N, M, R, C, L = map(int, input().split())
     tunel = [list(map(int,input().split())) for _ in range(N)]
     visited = [[0]* M for _ in range(N)]
+    visited[R][C] = 1
     q = deque([])
     ways = {
         1 : [[1,0],[-1,0],[0,1],[0,-1]],
         2 : [[1,0],[-1,0]],
         3 : [[0,1],[0,-1]],
         4 : [[-1,0], [0,1]],
-        5 : [[1,0], [0,-1]],
+        5 : [[1,0], [0,1]],
         6 : [[1,0],[0,-1]],
         7 : [[-1,0],[0,-1]],
     }
@@ -40,6 +39,8 @@ for tc in range(1, T+1):
     cnt =0
     for row in range(N):
         for col in range(M):
-            if visited[row][col]:
-               cnt +=1     
+            if 0< visited[row][col] <= L:
+               cnt +=1 
+    print(f'#{tc}',end=' ')    
     print(cnt)
+    # print(visited)
